10bc448c53e523ddb8a2447d37019ed5869225eb
AMDevG <jeberry308@gmail.com>
2017-10-28 23:35:00 -0500

    Sending&Receiving from Server; Need to Update Map; Need to parse Json and fill in textView

--- Ark/ViewController.swift
+++ Ark/ViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import MapKit
 //import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
+    
     var victim_array: [Victim] = []
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var textView: UITextView!
     
     @IBAction func sendSavedButtonPress(_ sender: AnyObject) {
+        print("Victim array has this many \(victim_array.count)")
         
-        var random_int = arc4random_uniform(UInt32(Int(victim_array.count))) + 1;
+        var random_int = arc4random_uniform(UInt32(Int(victim_array.count - 1))) + 1;
         
         print("Random int is: \(random_int)")
         var selectedVictim = victim_array[Int(random_int)]
@@ -22,9 +27,9 @@ class ViewController: UIViewController {
         print("Selected victim \(selectedVictim)")
         
         downloadPredictions(victim: selectedVictim)
+        victim_array.remove(at: Int(random_int))
+        
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,6 +58,7 @@ class ViewController: UIViewController {
         let readableJSON = try! JSONSerialization.jsonObject(with: jsonData! as Data, options: []) as![String:AnyObject]
         
         print("JSON Returned is \(readableJSON)")
+        
         //let object = JSON(readableJSON)
         
         
@@ -60,40 +66,40 @@ class ViewController: UIViewController {
     
     func createVictimObjects(){
         
-        let victim1 = Victim(name: "David", victimID: "v1", lat:29.676784, lon:-95.053939, group_size:2)
+        let victim1 = Victim(name: "David", victimID: "v1", lat:29.676784, lon:-95.051149, group_size:2)
         victim_array.append(victim1)
         
-        let victim2 = Victim(name: "Gina", victimID: "v2", lat:29.676784, lon:-95.053939, group_size:1)
+        let victim2 = Victim(name: "Gina", victimID: "v2", lat:29.683719, lon:-95.053939, group_size:1)
         victim_array.append(victim2)
         
-        let victim3 = Victim(name: "Estaban", victimID: "v3", lat:29.676784, lon:-95.053939, group_size:3)
+        let victim3 = Victim(name: "Estaban", victimID: "v3", lat:29.695963, lon:-95.106995, group_size:3)
         victim_array.append(victim3)
         
-        let victim4 = Victim(name: "John", victimID: "v4", lat:29.676784, lon:-95.053939, group_size:4)
+        let victim4 = Victim(name: "John", victimID: "v4", lat:29.585855, lon:-95.063736, group_size:4)
         victim_array.append(victim4)
         
-        let victim5 = Victim(name: "Ben", victimID: "v5", lat:29.676784, lon:-95.053939, group_size:2)
+        let victim5 = Victim(name: "Ben", victimID: "v5", lat:29.515594, lon:-95.065367, group_size:2)
         victim_array.append(victim5)
         
-        let victim6 = Victim(name: "Abi", victimID: "v6", lat:29.676784, lon:-95.053939, group_size:4)
+        let victim6 = Victim(name: "Abi", victimID: "v6", lat:29.670535, lon:-95.177290, group_size:4)
         victim_array.append(victim6)
         
-        let victim7 = Victim(name: "Jenny", victimID: "v7", lat:29.676784, lon:-95.053939, group_size:3)
+        let victim7 = Victim(name: "Jenny", victimID: "v7", lat:29.750136, lon:-95.231050, group_size:3)
         victim_array.append(victim7)
         
-        let victim8 = Victim(name: "Akshay", victimID: "v8", lat:29.676784, lon:-95.053939, group_size:3)
+        let victim8 = Victim(name: "Akshay", victimID: "v8", lat:29.656796, lon:-95.203584, group_size:3)
         victim_array.append(victim8)
         
-        let victim9 = Victim(name: "Gowen Family", victimID: "v9", lat:29.676784, lon:-95.053939, group_size:1)
+        let victim9 = Victim(name: "Gowen Family", victimID: "v9", lat:29.825520, lon:-95.142129, group_size:1)
         victim_array.append(victim9)
         
-        let victim10 = Victim(name: "Ricky", victimID: "v10", lat:29.676784, lon:-95.053939, group_size:3)
+        let victim10 = Victim(name: "Ricky", victimID: "v10", lat:29.804966, lon:-95.239976, group_size:3)
         victim_array.append(victim10)
         
-        let victim11 = Victim(name: "William", victimID: "v11", lat:29.676784, lon:-95.053939, group_size:1)
+        let victim11 = Victim(name: "William", victimID: "v11", lat:29.787090, lon:-95.262721, group_size:1)
         victim_array.append(victim11)
         
-        let victim12 = Victim(name: "Maggie", victimID: "v12", lat:29.676784, lon:-95.053939, group_size:3)
+        let victim12 = Victim(name: "Maggie", victimID: "v12", lat:29.704222, lon:-95.382541, group_size:3)
         victim_array.append(victim12)
     }
     
